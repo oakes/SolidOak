e0fe452e3297cc103f077cb1a95ba128974a3975
oakes <zsoakes@gmail.com>
2014-10-04 08:36:21 -0400

    Use GTK+

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![feature(globs)]
+
+extern crate rgtk;
+
+use rgtk::*;
+use rgtk::gtk::signals;
+
 fn main() {
-    println!("Hello, world!")
+    gtk::init();
+
+    let mut window = gtk::Window::new(gtk::window_type::TopLevel).unwrap();
+    window.set_title("SolidOak");
+    window.set_border_width(10);
+    window.set_window_position(gtk::window_position::Center);
+    window.set_default_size(1242, 768);
+
+    window.connect(signals::DeleteEvent::new(|_| {
+        gtk::main_quit();
+        true
+    }));
+
+    window.show_all();
+    gtk::main();
 }
