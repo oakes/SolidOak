0547cd3cfe6c4a3a08dd155ad61bbbdabcce69b9
oakes <zsoakes@gmail.com>
2014-10-05 23:46:48 -0400

    Remove or rename size variables

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ fn main() {
 
     let width = 1242;
     let height = 768;
-    let sidebar_width = ((width as f32) * 0.20) as i32;
-    let top_height = ((height as f32) * 0.8) as i32;
+    let editor_height = ((height as f32) * 0.8) as i32;
 
     let mut window = gtk::Window::new(gtk::window_type::TopLevel).unwrap();
     window.set_title("SolidOak");
@@ -36,13 +35,13 @@ fn main() {
 
     let mut project_pane = gtk::Box::new(gtk::orientation::Vertical, 0).unwrap();
     let project_tree = gtk::TreeView::new().unwrap();
-    project_pane.set_size_request(sidebar_width, -1);
+    project_pane.set_size_request(-1, -1);
     project_pane.pack_start(&project_buttons, false, true, 0);
     project_pane.pack_start(&project_tree, true, true, 0);
 
     let editor_pane = gtk::TextView::new().unwrap();
     let build_pane = gtk::TextView::new().unwrap();
-    editor_pane.set_size_request(-1, top_height);
+    editor_pane.set_size_request(-1, editor_height);
     let mut content = gtk::Box::new(gtk::orientation::Vertical, 0).unwrap();
     content.pack_start(&editor_pane, false, true, 0);
     content.pack_start(&build_pane, true, true, 0);
