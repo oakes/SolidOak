8334b40f87b318ef1339a79ed2102cb180e9bcf0
oakes <zsoakes@gmail.com>
2014-10-05 23:38:54 -0400

    Remove rusti pane and improve pane sizes

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ fn main() {
 
     let width = 1242;
     let height = 768;
-    let sidebar_width = ((width as f32) * 0.32) as i32;
-    let content_width = width - sidebar_width;
+    let sidebar_width = ((width as f32) * 0.20) as i32;
     let top_height = ((height as f32) * 0.8) as i32;
 
     let mut window = gtk::Window::new(gtk::window_type::TopLevel).unwrap();
@@ -29,34 +28,28 @@ fn main() {
     let remove_button = gtk::Button::new_with_label("Remove").unwrap();
 
     let mut project_buttons = gtk::Box::new(gtk::orientation::Horizontal, 0).unwrap();
-    project_buttons.set_size_request(sidebar_width, -1);
+    project_buttons.set_size_request(-1, -1);
     project_buttons.add(&new_project_button);
     project_buttons.add(&import_button);
     project_buttons.add(&rename_button);
     project_buttons.add(&remove_button);
 
-    let project_tree = gtk::TreeView::new().unwrap();
-
     let mut project_pane = gtk::Box::new(gtk::orientation::Vertical, 0).unwrap();
-    project_pane.set_size_request(sidebar_width, top_height);
+    let project_tree = gtk::TreeView::new().unwrap();
+    project_pane.set_size_request(sidebar_width, -1);
     project_pane.pack_start(&project_buttons, false, true, 0);
     project_pane.pack_start(&project_tree, true, true, 0);
 
-    let rusti_pane = gtk::TextView::new().unwrap();
-    let mut vbox_1 = gtk::Box::new(gtk::orientation::Vertical, 0).unwrap();
-    vbox_1.pack_start(&project_pane, false, true, 0);
-    vbox_1.pack_start(&rusti_pane, true, true, 0);
-
     let editor_pane = gtk::TextView::new().unwrap();
     let build_pane = gtk::TextView::new().unwrap();
-    editor_pane.set_size_request(content_width, top_height);
-    let mut vbox_2 = gtk::Box::new(gtk::orientation::Vertical, 0).unwrap();
-    vbox_2.pack_start(&editor_pane, false, true, 0);
-    vbox_2.pack_start(&build_pane, true, true, 0);
+    editor_pane.set_size_request(-1, top_height);
+    let mut content = gtk::Box::new(gtk::orientation::Vertical, 0).unwrap();
+    content.pack_start(&editor_pane, false, true, 0);
+    content.pack_start(&build_pane, true, true, 0);
 
     let mut hbox = gtk::Box::new(gtk::orientation::Horizontal, 0).unwrap();
-    hbox.pack_start(&vbox_1, false, true, 0);
-    hbox.pack_start(&vbox_2, true, true, 0);
+    hbox.pack_start(&project_pane, false, true, 0);
+    hbox.pack_start(&content, true, true, 0);
     window.add(&hbox);
 
     window.show_all();
