a5ab2ebb46713b16d03a61347598c5a1454b8014
oakes <zsoakes@gmail.com>
2015-02-15 23:42:55 -0500

    Update libraries

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ use std::old_io::Command;
 use std::env;
 
 fn main() {
-    let curr_dir = Path::new(env::var("CARGO_MANIFEST_DIR").unwrap().into_string().unwrap());
+    let curr_dir = Path::new(env::var("CARGO_MANIFEST_DIR").unwrap());
     Command::new("git").arg("submodule").arg("update").arg("--init").cwd(&curr_dir).status().unwrap();
 }
