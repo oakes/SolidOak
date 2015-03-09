da65f9ec930f67bddfde643fcaa1c31a925e7cbb
oakes <zsoakes@gmail.com>
2015-03-09 00:37:19 -0400

    Remove racer vimscript temporarily

--- src/utils.rs
+++ src/utils.rs
@@ -54,8 +54,6 @@ pub static DATA_CONTENT : &'static [Resource] = &[
 
     Resource{path: &["plugin", "eunuch.vim"],
              data: include_str!("../resources/soak/plugin/eunuch.vim")},
-    Resource{path: &["plugin", "racer.vim"],
-             data: include_str!("../resources/soak/plugin/racer.vim")},
     Resource{path: &["plugin", "rust.vim"],
              data: include_str!("../resources/soak/plugin/rust.vim")},
 
