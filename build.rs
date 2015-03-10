9137f0b0e4ff74ceeed28fa0870a99303c22c8b2
oakes <zsoakes@gmail.com>
2015-03-09 22:41:02 -0400

    Silence warnings

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
+#![feature(path)] 
+
+use std::path::Path;
 use std::process::Command;
 use std::env;
 
 fn main() {
-    let curr_dir = Path::new(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let curr_path_str = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let curr_path = Path::new(&curr_path_str);
     Command::new("git").arg("submodule").arg("update").arg("--init")
-        .current_dir(&curr_dir).status().unwrap();
+        .current_dir(&curr_path).status().unwrap();
 }
