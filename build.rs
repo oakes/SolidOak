b71cfd5b6f41f8436df8e2cf6c895812d9a2bbdf
oakes <zsoakes@gmail.com>
2016-02-14 10:38:28 -0500

    Simplify build script

--- build.rs
+++ build.rs
@@ -1,10 +1,8 @@
-use std::path::Path;
 use std::process::Command;
 use std::env;
 
 fn main() {
-    let curr_path_str = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let curr_path = Path::new(&curr_path_str);
+    let curr_dir = env::current_dir().unwrap();
     Command::new("git").arg("submodule").arg("update").arg("--init")
-        .current_dir(&curr_path).status().unwrap();
+        .current_dir(&curr_dir).status().unwrap();
 }
